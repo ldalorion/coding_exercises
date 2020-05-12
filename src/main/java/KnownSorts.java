5f8d9c989f6d7ff76aa70e17030a33f5feb5ec41
dalorion <ldalorion@gmail.com>
2020-05-11 20:46:29 -0500

    added a couple of more sorts and print statements

--- src/main/java/KnownSorts.java
+++ src/main/java/KnownSorts.java
@@ -1,7 +1,7 @@
 public class KnownSorts {
 
     /**
-     *The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order)
+     * The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order)
      * from unsorted part and putting it at the beginning. The algorithm maintains two subarrays in a given array.
      * https://www.geeksforgeeks.org/selection-sort/
      *
@@ -35,7 +35,7 @@ public class KnownSorts {
      * @param r ending index section of interest
      */
     void insertion(int[] a, int l, int r) {
-        CodeExerciseUtil.printArray("Current Array ", a, false);
+        CodeExerciseUtil.printArray("In the beginning ", a, true);
         int i;
         // starting at the end of the array, move the smallest number all the way to
         // the beginning
@@ -43,14 +43,80 @@ public class KnownSorts {
             SortUtils.compareAndExchange(a, i - 1, i);
         }
         for (i = l + 2; i <= r; i++) {
+            CodeExerciseUtil.printArray("2nd for loop ", a, true);
             int j = i;
             int v = a[i];
+            System.out.println("a[" + i + "]: " + v);
+            System.out.println("a[" + (j - 1) + "]: " + a[(j - 1)]);
+            System.out.println("SortUtils.less(" + v + ", " + a[(j - 1)] + ")");
             while (SortUtils.less(v, a[j - 1])) {
+                System.out.println("-----WHILE-----");
+                System.out.println("a[" + (j - 1) + "]: " + a[(j - 1)]);
+                System.out.println("a[" + j + "]: " + a[j]);
                 a[j] = a[j - 1];
+                System.out.println("a[" + (j - 1) + "]: " + a[(j - 1)]);
+                System.out.println("a[" + j + "]: " + a[j]);
                 j--;
+                System.out.println("SortUtils.less(" + v + ", " + a[(j - 1)] + ")");
+                System.out.println("-----WHILE-----");
             }
             a[j] = v;
-            CodeExerciseUtil.printArray("Current Array ", a, false);
+            System.out.println("a[j] = v");
+            System.out.println("a[" + j + "]: " + v);
+        }
+    }
+
+    /**
+     * Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
+     * https://www.geeksforgeeks.org/bubble-sort/
+     *
+     * @param a array to sort
+     * @param l beginning index section of interest
+     * @param r ending index section of interest
+     */
+    void bubble(int[] a, int l, int r) {
+        for (int i = l; i < r; i++) {
+            for (int j = r; j > i; j--) {
+                SortUtils.compareAndExchange(a, j - 1, j);
+            }
+        }
+    }
+
+    /**
+     * Nonadaptive insertion sort.
+     *
+     * @param a array to sort
+     * @param l beginning index section of interest
+     * @param r ending index section of interest
+     */
+    void insertionSortExchangedBased(int[] a, int l, int r) {
+        for (int i = l + 1; i <= r; i++) {
+            for (int j = i; j > l; j--) {
+                SortUtils.compareAndExchange(a, j - 1, j);
+            }
+        }
+    }
+
+    /**
+     * One way to implement shellsort would be, for each h, to use insertion sort independently on each of the h subfiles.
+     *
+     * @param a array to sort
+     * @param l beginning index section of interest
+     * @param r ending index section of interest
+     */
+    void shell(int[] a, int l, int r) {
+        int h;
+        for (h = 1; h <= (r - 1) / 9; h = 3 * h + 1) ;
+        for (; h > 0; h /= 3) {
+            for (int i = 1 + h; i <= r; i++) {
+                int j = i;
+                int v = a[i];
+                while (j >= l + h && SortUtils.less(v, a[j - h])) {
+                    a[j] = a[j - h];
+                    j -= h;
+                }
+                a[j] = v;
+            }
         }
     }
 }
